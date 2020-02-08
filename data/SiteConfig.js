3554ee4103bf10c0c38235fccb189f6cd548f5d5
Toqeer Iqbal <toqeer.94@gmail.com>
2020-02-08 19:29:48 +0500

    Theme Update

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,18 +1,16 @@
 const config = {
-  siteTitle: "Toqeer Iqbal", // Site title.
-  siteTitleShort: "Toqeer Iqbal", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
-  siteTitleAlt: "Toqeer Iqbal", // Alternative site title for SEO.
+  siteTitle: "Gatsby Advanced Starter", // Site title.
+  siteTitleShort: "GA Starter", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
+  siteTitleAlt: "GatsbyJS Advanced Starter", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://toqeeriqbal.com", // Domain of your website without pathPrefix.
+  siteUrl: "https://gatsby-advanced-starter-demo.netlify.com", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  author: "@toqeer",
-  siteDescription:
-    "I am Toqeer, MERN Stack Developer from Pakistan. Currently Working with Two Big Travel Brands TheBrokeBackPacker & OneWeirdGlobe. I have over 4 years of experience in Web Development.", // Website description used for RSS feeds/meta description tag.
+  siteDescription: "A GatsbyJS stater with Advanced design in mind.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteRssTitle: "Gatsby Advanced Starter RSS feed", // Title of the RSS feed
-  siteFBAppID: "", // FB Application ID for using app insights
-  googleAnalyticsID: "", // GA tracking ID.
-  disqusShortname: "", // Disqus shortname.
+  siteFBAppID: "1825356251115265", // FB Application ID for using app insights
+  googleAnalyticsID: "UA-47311644-5", // GA tracking ID.
+  disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // Disqus shortname.
   dateFromFormat: "YYYY-MM-DD", // Date format used in the frontmatter.
   dateFormat: "DD/MM/YYYY", // Date format for display.
   postsPerPage: 4, // Amount of posts displayed per listing page.
@@ -28,40 +26,40 @@ const config = {
     {
       label: "GitHub",
       url: "https://github.com/Vagr9K/gatsby-advanced-starter",
-      iconClassName: "fa fa-github",
+      iconClassName: "fa fa-github"
     },
     {
       label: "Twitter",
       url: "https://twitter.com/Vagr9K",
-      iconClassName: "fa fa-twitter",
+      iconClassName: "fa fa-twitter"
     },
     {
       label: "Email",
       url: "mailto:vagr9k@gmail.com",
-      iconClassName: "fa fa-envelope",
-    },
+      iconClassName: "fa fa-envelope"
+    }
   ],
   copyright: "Copyright © 2019. Advanced User", // Copyright string for the footer of the website and RSS feed.
-  themeColor: "#663399", // Used for setting manifest and progress theme colors.
-  backgroundColor: "#663399", // Used for setting manifest background color.
-}
+  themeColor: "#c62828", // Used for setting manifest and progress theme colors.
+  backgroundColor: "#e0e0e0" // Used for setting manifest background color.
+};
 
 // Validate
 
 // Make sure pathPrefix is empty if not needed
 if (config.pathPrefix === "/") {
-  config.pathPrefix = ""
+  config.pathPrefix = "";
 } else {
   // Make sure pathPrefix only contains the first forward slash
-  config.pathPrefix = `/${config.pathPrefix.replace(/^\/|\/$/g, "")}`
+  config.pathPrefix = `/${config.pathPrefix.replace(/^\/|\/$/g, "")}`;
 }
 
 // Make sure siteUrl doesn't have an ending forward slash
 if (config.siteUrl.substr(-1) === "/")
-  config.siteUrl = config.siteUrl.slice(0, -1)
+  config.siteUrl = config.siteUrl.slice(0, -1);
 
 // Make sure siteRss has a starting forward slash
 if (config.siteRss && config.siteRss[0] !== "/")
-  config.siteRss = `/${config.siteRss}`
+  config.siteRss = `/${config.siteRss}`;
 
-module.exports = config
+module.exports = config;
